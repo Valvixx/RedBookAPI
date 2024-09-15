3fe9e94436e629adb7d1012280100116e63cec00
MR1meteor <82905956+MR1meteor@users.noreply.github.com>
2024-09-15 20:32:48 +0300

    Move changes

--- api/Application/Services/Interfaces/IBookElementService.cs
+++ api/Application/Services/Interfaces/IBookElementService.cs
@@ -1,4 +1,5 @@
 ﻿using Application.DTO.BookElement;
+using Application.Services.Models;
 using Domain.Entities;
 
 namespace Application.Services;
@@ -7,8 +8,8 @@ public interface IBookElementService
 {
     // CRUD. Делать апдейт через patch
     Task<BookElement> CreateAsync(BookElementCreate data);
-    Task<BookElement> UpdateAsync(int id, BookElementCreate data);
-    Task DeleteAsync(int id);
-    
-
+    Task<BookElement> UpdateAsync(int id, BookElementUpdate data);
+    Task<BookElement> DeleteAsync(int id);
+    Task<BookElement> GetAllByType(BookElementType type);
+    Task<BookElement> GetById(int id);
 }
